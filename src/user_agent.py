31f1c49865160310bf0251afdee87ba9643424a5
shemhazai <andre.marques@fatec.sp.gov.br>
2017-01-31 08:31:03 -0200

    Added set length checkage

--- src/user_agent.py
+++ src/user_agent.py
@@ -7,4 +7,6 @@ def get_user_agents():
     with open(json_file) as data_file:
         data = json.load(data_file)
     user_agents = [x["agent_string"] for x in data]
+    if len(user_agents) < 1:
+        return None
     return user_agents
