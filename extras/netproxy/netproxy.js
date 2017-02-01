11eb69fdf61eb12313c949dec1d12a80c1e23b4a
shemhazai <andre.marques@fatec.sp.gov.br>
2017-02-01 20:18:07 -0200

    Added netproxy to help with testing websites for cracking passwords

---
+++ extras/netproxy/netproxy.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var url = require('url');
+http.createServer(function(request, response) {
+      console.log(request.method + " " + request.url);
+      var url_parts = url.parse(request.url);
+      var proxy = http.createClient(80, request.headers['host'])
+      var proxy_request = proxy.request(request.method, request.url, request.headers);
+      proxy_request.on('response', function (proxy_response) {
+          
+          proxy_response.on('data', function(chunk) {
+              response.write(chunk, 'binary');
+          });
+
+          proxy_response.on('end', function() {
+              response.end();
+          });
+          
+          response.writeHead(proxy_response.statusCode, proxy_response.headers);
+      });
+
+      request.on('data', function(chunk) {
+          console.log(chunk.toString('utf-8') + "\n");
+          proxy_request.write(chunk, 'binary');
+      });
+      request.on('end', function() {
+          proxy_request.end();
+      });
+}).listen(8080, function() {
+    console.log("NetProxy listening on *:8080");    
+});
