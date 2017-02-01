e3de144f27bfb39c15668792fb717e0b18924e2b
shemhazai <andre.marques@fatec.sp.gov.br>
2017-02-01 09:14:05 -0200

    Fixed function restore_script() to work with execution permissions

--- src/resume.py
+++ src/resume.py
@@ -5,18 +5,15 @@ from netbruter.argv import retrieve_command_line
 def restore_script(script_file="resume.sh"):
     """
     If 'resume.sh' does not exists, create it to easily resume the attack
+    If it exists, delete it.
     :param script_file:
     :return: None
     """
     if os.path.isfile(script_file):
-        return None
+        os.remove(script_file)
 
     data = "#!/bin/bash\n{0}".format(retrieve_command_line())
     with open(script_file, "w") as f:
         f.write(data)
-    try:
-        os.chmod(os.path.abspath(script_file), 755)
-    except:
-        print("[!] Error: Could not change permissions to resume script.")
-    print("[+] Resume script generated and stored at '{0}'".format(script_file))
+    os.chmod(script_file, 0o755)
     return True
